5f47cedc5ef83c21d51f264c295870f6db30b3b2
IKQR <kusikillya2001@gmail.com>
2022-06-19 20:39:14 +0300

    base view for restaurant children

---
+++ src/Web/Client/Pages/Admin/RestaurantChildBaseComponent.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using QRCodeMenu.Shared.Dto;
+using System.Net.Http.Json;
+using static QRCodeMenu.Client.Components.Diver;
+
+namespace QRCodeMenu.Client.Pages.Admin
+{
+    public partial class RestaurantChildBaseComponent
+    {
+        [Inject]
+        protected HttpClient Http { get; init; } = null!;
+        [Inject]
+        protected IJSRuntime JSRuntime { get; init; } = null!;
+
+        [Parameter]
+        public int RestaurantId { get; set; } = 0;
+        protected RestaurantDto Restaurant { get; set; } = new();
+
+
+        private DiverItem[] DiverItems => new DiverItem[] {
+            new("Restaurants", "/admin/restaurant"),
+            new(Restaurant.Name, null),
+        };
+        protected virtual DiverItem[] DiverSubItems => new DiverItem[] { };
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadRestaurant();
+            await LoadOnInitialized();
+
+            await base.OnInitializedAsync();
+        }
+
+        protected virtual async Task LoadOnInitialized()
+        {
+            await Task.CompletedTask;
+        }
+
+        protected async Task LoadRestaurant()
+        {
+            var result =
+                await Http.GetFromJsonAsync<RestaurantDto>(
+                    $"/api/admin/restaurant/{RestaurantId}")
+                ?? new();
+
+            Restaurant = result;
+        }
+    }
+}
